5fc0423c4e3b7b9546e7eb4e5a9384715b73742a
Likhith <likhithsai7979@gmail.com>
2024-07-03 10:50:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+export const WeatherApp = () => {
+  const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    if (!location) {
+      setWeather(null);
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.weatherapi.com/v1/current.json?key=c795a89574e0479680073112242802&q=${location}`
+      );
+      setWeather(response.data);
+      console.log(response.data);
+    } catch (error) {
+      alert(`Failed to fetch weather data: ${error.message}`);
+      setWeather(null);
+    }
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-child">
+        <h1>Weather App</h1>
+        <input
+          type="text"
+          placeholder="Enter Location Here"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button type="submit" onClick={fetchData}>
+          Submit
+        </button>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          weather && (
+            <div>
+              <div className="weather-box">
+                <p>Temperature: {weather.current.temp_c}°C</p>
+              </div>
+              <div className="weather-box">
+                <p>Condition: {weather.current.condition.text}</p>
+              </div>
+              <div className="weather-box">
+                <p>Humidity: {weather.current.humidity}%</p>
+              </div>
+              <div className="weather-box">
+                <p>Wind Speed: {weather.current.wind_kph} kph</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default WeatherApp;
